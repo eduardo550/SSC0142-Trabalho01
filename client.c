85b10ded4c7c0056da1e669670b789fc2add7d1d
eduardo550 <eduardo.sousa.siqueira@gmail.com>
2021-06-05 19:25:29 -0300

    Basic server/client functionality

--- client.c
+++ client.c
@@ -1,5 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 4206
 
 int main(int argc, char *argv[]) {
 
+    if(argc < 2) {
+        printf("Usage: %s ServerIP\n", argv[0]);
+        return 0;
+    }
+
+    // Criar socket
+    int sock;
+    struct sockaddr_in server_address;
+    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        printf("Error creating socket. Aborting\n");
+        return 1;
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    //argv[1] contem o endereco IP do servidor, fornecido na linha de comando
+    if(inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0) {
+        printf("Not a valid IP address. Aborting.\n");
+        return 1;
+    }
+
+    // Fazendo a conexao
+    if(connect(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+        printf("Connection failed. Aborting.\n");
+        return 1;
+    }
+    printf("Connected.\n");
+    // Criar funcao para troca de msgs
+
+    close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
+
+#define PORT 4206
+//troca de msgs
 
 int main(int argc, char *argv[]) {
 
+    // Criar socket
+    int serv_sock = -1, incoming_sock, incoming_len;
+    struct sockaddr_in server_address, client_address;
+
+    if((serv_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating socket. Aborting.\n");
+        return 1;
+    }
+    
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+
+    // Bind
+    if(bind(serv_sock, (struct sockaddr *) &server_address, sizeof(server_address)) != 0) {
+        printf("Error binding socket.\n");
+        return 1;
+    }
+
+    // Listen
+    if(listen(serv_sock, 10) != 0) {  //Backlog de 10 conexoes
+        printf("Failed to listen to connections.\n");
+        return 1;
+    }
+    printf("Server online and awaiting connections.\n");
+
+    // Accept
+    while(1) {
+        incoming_sock = accept(serv_sock, (struct sockaddr *) &client_address, &incoming_len);
+        if(incoming_sock < 0) {
+            printf("Connection failed to accept.\n");
+        } else {
+        // fazer a troca de msgs em uma thread
+            printf("Connected to %s.\n", inet_ntoa(client_address.sin_addr));
+        }
+    }
+
+    close(serv_sock);
     return 0;
 }
