19979a815f1c86efd209a12b83ed0da61ea5786d
eduardo550 <eduardo.sousa.siqueira@gmail.com>
2021-06-08 20:41:07 -0300

    Versao final

--- client.c
+++ client.c
@@ -1,91 +1,254 @@
-#include <stdlib.h>
+/* Trabalho 01 - Servidor FTP Simples
+	Eduardo de Sousa Siqueira		9278299
+	Igor Barbosa Grécia Lúcio		9778821
+*/
+
 #include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <errno.h>
 
-#define PORT 4206
+//Maximo de bytes que sao enviados de uma vez
+#define MAX 80
+//Flags para funcoes send() e recv()
 #define FLAGS 0
+#define SA struct sockaddr
 
-int handle_connection(int sockfd) {
-    //Exemplo comunicaçao
-    // ssize_t len;
-    // char *msg = "123";
-    // int flags = 0;
-    // len = send(sockfd, msg, 4, FLAGS);
+//Funcao que trata o envio de um arquivo, em chunks de MAX == 80 bytes
+void send_file(FILE *fp, int sockfd) {
+	char data[MAX] = {0};
 
-    char username[17]; //16 + '\0'
-    /*Interface:
-        Inicialemente pergunta se quer criar uma conta ou logar, manda a op pro servidor (0 criar, 1 logar)
-        Conta só tem username, de no max 16 chars
-        Mandar o username, servidor retorna strlen(username) se sucesso, -1 se a conta existe no caso de criar, ou se n existe no caso de logar
-        Depois, um prompt com opçoes
-            1. Listar os arquivos no server
-            2. Mandar um arquivo para o sr
-            3. Copiar um arquivo do server
-            4. Remover um arquivo do server
-            5. Remover todos os arquivos do server
-            6. Remover a conta
-            7. Sair
-        Faz um send com o n da opcao, e outro com o arg, se necessario
-        Args:
-            1. Nenhum
-            2. O arquivo
-            3. O nome do arquivo
-            4. O nome do arquivo
-            5. Nenhum
-            6. Nenhum
-            7. Nenhum, termina a conexao e programa acaba
-        Servidor retorna:
-            1. 0 (sucesso) ou -1 (fail)
-            2. n_bytes (sucesso) ou -1 (fail)
-            3. o arquivo (sucesso) ou -1 (fail)
-            4. 0 (sucesso) ou -1 (fail)
-            5. 0 (sucesso) ou -1 (fail)
-            6. 0 (sucesso) ou -1 (fail)
-            7. Nada, programa acaba
-    */
+	//Lendo arquivo em segmentos de MAX, até o final
+	while(fgets(data, MAX, fp) != NULL) {
+		if (send(sockfd, data, sizeof(data), FLAGS) == -1) {
+			perror("[-]Error in sending file.");
+			exit(1);
+		}
+		
+		bzero(data, MAX);
+	}
+}
 
-    //Case 7
-    //Um print aqui tbm
-    close(sockfd);
-    return 0;
+void func(int sockfd) {
+	char buff[MAX];
+	char name[MAX];
+	int n;
+	int retval;
+	FILE *fp;
+	for(;;){
+		printf("Criar Conta(0), Login(1) ou Sair do Programa(7)\n");
+		bzero(buff, sizeof(buff));
+		n = 0;
+		//Lendo a opcao escolhida e enviando para o servidor
+		while ((buff[n++] = getchar()) != '\n');
+		send(sockfd, buff, sizeof(buff), FLAGS);
+
+		//Opção 1 - Fazer Login
+		if (strcmp(buff, "1\n") == 0) {
+			printf("Nome de usuario: ");
+			for(;;){
+				//Lendo o nome de usuario
+				bzero(buff, sizeof(buff));
+				bzero(name, sizeof(name));
+				n = 0;
+				while ((buff[n++] = getchar()) != '\n');
+
+				//Copiando para o buffer, sem a quebra de linha, e enviando para o servidor
+				strncpy(name, buff, n-1);
+				send(sockfd, name, sizeof(name), FLAGS);
+				recv(sockfd, &retval, sizeof(int), FLAGS);
+
+				//Recebido sucesso, entra na interface principal
+				if (retval != -1) {
+					printf("Logado\n\n");
+					//Loop de interface principal
+					for(;;) {
+						printf("Escolha uma opção:\n"
+							"\t1. Listar Arquivos\n"
+							"\t2. Upload um Arquivo\n"
+							"\t3. Download um Arquivo\n"
+							"\t4. Remover um Arquivo do Servidor\n"
+							"\t5. Remover Todos os seus Arquivos\n"
+							"\t6. Remover sua Conta\n"
+							"\t7. Logout\n"
+						);
+						//Lendo a opçao e enviando para o servidor
+						bzero(buff, sizeof(buff));
+						n = 0;
+						while ((buff[n++] = getchar()) != '\n');
+						send(sockfd, buff, sizeof(buff), FLAGS);
+
+						//Option Selector 1 - List Files
+						if (strncmp(buff, "1\n", 2) == 0){
+							//Lendo e imprimindo a lista de arquivos contidos no server
+							read(sockfd, buff, sizeof(buff));
+							while(strncmp(buff, "-1", 2) != 0){
+								printf("%s\n", buff);
+								bzero(buff, sizeof(buff));
+								read(sockfd, buff, sizeof(buff));
+							}
+						}
+						//Option Selector 2 - Send File to Server
+						else if (strncmp(buff, "2\n", 2) == 0) {
+							printf("Nome do arquivo: ");
+							//Lendo o nome do arquivo
+							bzero(name, sizeof(name));
+							bzero(buff, sizeof(buff));
+							n = 0;
+							while ((buff[n++] = getchar()) != '\n');
+							//Copiando para o buffer sem a quebra de linha
+							strncpy(name, buff, n-1);
+							fp = fopen(name, "r");
+
+							//Enviando o nome do arquivo para o servidor
+							if (fp == NULL) {
+								perror("[-]Error in reading file.");
+								char *errval = "\\\\//";
+								send(sockfd, errval, strlen(errval)+1, FLAGS);
+							} else {
+								send(sockfd, name, sizeof(name), 0);
+								//Enviando o conteudo do arquivo
+								send_file(fp, sockfd);
+								fclose(fp);
+								printf("%s enviado\n", name);
+							}
+						}
+						//Option Selector 3 - Copy File from Server
+						else if (strncmp(buff, "3\n", 2) == 0) {
+							printf("Nome do arquivo: ");
+							//Lendo o nome do arquivo
+							bzero(name, sizeof(name));
+							bzero(buff, sizeof(buff));
+							n = 0;
+							while ((buff[n++] = getchar()) != '\n');
+							//Copiando para o buffer sem a quebra de linha
+							strncpy(name, buff, n-1);
+
+							send(sockfd, name, sizeof(name), FLAGS);
+							recv(sockfd, buff, sizeof(buff), FLAGS);
+
+							//Tratando a resposta do servidor
+							if(!strcmp(buff, "Not found")) {
+								printf("Arquivo nao foi encontrado no servidor.\n");
+							} else {
+								fp = fopen(name, "w");
+								fprintf(fp, "%s", buff);
+								fclose(fp);
+								printf("Recebido\n");
+							}
+						}
+						//Option Selector 4 - Delete Single File
+						else if (strncmp(buff, "4\n", 2) == 0) {
+							//Lendo o nome do arquivo a ser deletado
+							printf("Nome do arquivo: ");
+							bzero(name, sizeof(name));
+							bzero(buff, sizeof(buff));
+							n = 0;
+							while ((buff[n++] = getchar()) != '\n');
+							strncpy(name, buff, n-1);
+							
+							send(sockfd, name, sizeof(name), FLAGS);
+						}
+						//Option Selector 5 - Delete All Files
+						else if (strncmp(buff, "5\n", 2) == 0) {
+							printf("Deleting All Files\n");
+						}
+						//Option Selector 6 - Delete Account + Logout
+						else if (strncmp(buff, "6\n", 2) == 0) {
+							printf("Deleting Account\n");
+							break;
+						}
+						//Option Selector 7 - Logout
+						else if (strncmp(buff, "7\n", 2) == 0) {
+							printf("Saindo...\n");
+							break;
+						}
+						else {
+							printf("Opcao nao reconhecida\n");
+						}
+					}
+					break;
+				}
+				else {
+					printf("Conta inexistente.\n");
+					break;
+				}
+			}
+		}
+		//Opção 0 - Criar Conta
+		else if (strcmp(buff, "0\n") == 0) {
+			printf("Nome de usuario: ");
+			bzero(buff, sizeof(buff));
+			bzero(name, sizeof(name));
+			n = 0;
+			//Lendo username
+			while ((buff[n++] = getchar()) != '\n')
+				;
+
+			//Enviando o username para o servidor
+			strncpy(name, buff, n-1);
+			send(sockfd, name, sizeof(name), FLAGS);
+			recv(sockfd, &retval, sizeof(int), FLAGS);
+			//Tratando erros
+			if (retval == -1) {
+				printf("\nConta ja existe, tente de novo.\n");
+			}
+			else {
+				printf("\nConta criada. Faca Login para continuar\n");
+			}
+		}
+		//Opção 7 - Sair do Programa
+		else if (strcmp(buff, "7\n") == 0) {
+			printf("Fechando Cliente...\n");
+			break;
+		}
+		else {
+			printf("Opcao invalida. Tente de novo\n");
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
+	if(argc != 3) {
+		printf("Usage:\n\t%s [ServerIP] [Port Number]\n", argv[0]);
+		exit(1);
+	}
+	int sockfd;
+	struct sockaddr_in servaddr;
 
-    if(argc < 2) {
-        printf("Usage: %s ServerIP\n", argv[0]);
-        return 0;
-    }
+	//Criando socket TCP
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	//Abortando o programa se o socket nao foi criado corretamente
+	if (sockfd == -1) {
+		printf("Error creating socket\n");
+		exit(1);
+	}
 
-    // Criar socket
-    int sock;
-    struct sockaddr_in server_address;
-    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-        printf("Error creating socket. Aborting\n");
-        return 1;
-    }
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    //argv[1] contem o endereco IP do servidor, fornecido na linha de comando
-    if(inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0) {
-        printf("Not a valid IP address. Aborting.\n");
-        return 1;
-    }
+	//Definindo o endereco e port recebidos na linha de comando
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servaddr.sin_port = htons(atoi(argv[2]));
 
-    // Fazendo a conexao
-    if(connect(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
-        printf("Connection failed. Aborting. %s\n", strerror(errno));
-        return 1;
-    }
-    printf("Connected.\n");
-    // Criar funcao para troca de msgs
-    handle_connection(sock);
+	//Conectando com o servidor
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+		printf("Connection with the server failed...\n");
+		exit(1);
+	}
 
+	//Funcao que trata a comunicacao entre esse cliente e o servidor
+	func(sockfd);
 
-    return 0;
+	//Fechando o socket apos a conexao
+	close(sockfd);
+
+	return 0;
 }
--- server.c
+++ server.c
@@ -1,26 +1,34 @@
-#include <stdlib.h>
+/* Trabalho 01 - Servidor FTP Simples
+	Eduardo de Sousa Siqueira		9278299
+	Igor Barbosa Grécia Lúcio		9778821
+*/
+
 #include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <pthread.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/stat.h>
 
-#define PORT 4206
+//Maximo de bytes que sao enviados de uma vez
+#define MAX 80
+//Flags para funcoes send() e recv()
 #define FLAGS 0
-//troca de msgs
+#define SA struct sockaddr
 
+//Funcao que cria o diretorio de um novo usuario
 int create_account(char *username) {
     char path[20] = "db/";
     struct stat s;
     strcat(path, username);
 
     if (stat(path, &s) != -1) {
-        printf("Account already exists.\n");
         return -1;
     } else {
         mkdir(path, 0777);
@@ -28,81 +36,216 @@ int create_account(char *username) {
     }
 }
 
-void handle_login(int sockfd) {
-    ssize_t user_len;
-    char username[17];
-    int username_option;
-    int errorcode = -1;
-    int retval;
-
-    //Recebendo o modo, criar conta ou login
-    recv(sockfd, &username_option, sizeof(int), FLAGS);
-    switch(username_option) {
-        case 0:
-            user_len = recv(sockfd, username, 17, FLAGS);
-            if(user_len == -1) {
-                send(sockfd, &errorcode, sizeof(int), FLAGS);
-            } else {
-                retval = create_account(username);
-                send(sockfd, &retval, sizeof(int), FLAGS);
-            }
-            break;
-        case 1:
-            user_len = recv(sockfd, username, 17, FLAGS);
-            break;
-        default:
-            send(sockfd, &errorcode, sizeof(int), FLAGS);
-            break;
-    }
-
+//Funcao para envio de arquivos para o cliente
+void send_file(FILE *fp, int sockfd){
+	char data[MAX] = {0};
+	while(fgets(data, MAX, fp) != NULL) {
+		if (send(sockfd, data, sizeof(data), 0) == -1) {
+			exit(1);
+		}
+		bzero(data, MAX);
+	}
 }
 
+//Funcao que deleta todos os arquivos pertencentes a um usuario
+void delete_files_from_user(char *path) {
+	DIR *d = opendir(path);
+	char file_path[40];
+	struct dirent *dir;
+
+	//Loop pegando todos os nomes de arquivos no diretorio e os removendo
+	while ((dir = readdir(d)) != NULL){
+		bzero(file_path, 40);
+		strcat(file_path, path);
+		strcat(file_path, "/");
+		strcat(file_path, dir->d_name);
+		remove(file_path);
+	}
+
+	closedir(d);
+}
+
+//Funcao que lida com a comunicacao com o programa cliente
 void *handle_connection(void *args) {
-    //Deixa a thread independente, para que quando a conexao terminar, ela libera seus recursos automaticamente
-    pthread_detach(pthread_self());
-    int sockfd = *((int *) args);
+	//Tornando a thread independente do programa principal. Ao final da conexao, ela libera seus recursos automaticamente
+	pthread_detach(pthread_self());
 
-    handle_login(sockfd);
+	char buff[MAX];
+	char path[20];
+	char file_path[40];
+	FILE *fp;
+	DIR *d, *p;
+	struct dirent *dir;
+	int retval;
+	int sockfd = *((int *) args);
 
-    pthread_exit(0);
+	//Loop infinito para receber as opcoes do cliente
+	for (;;) {
+		bzero(buff, MAX);
+		//Lendo a opcao do cliente - login, criar conta ou sair
+		read(sockfd, buff, sizeof(buff));
+
+		// 1 - Fazer Login
+		if (strncmp("1\n", buff, 2) == 0) {
+			bzero(buff, MAX);
+			bzero(path, 20);
+			bzero(file_path, 40);
+			strcat(path,"db/");
+			read(sockfd, buff, sizeof(buff));
+			strcat(path, buff);
+			p = opendir(path);
+			if (p) {
+				write(sockfd, buff, sizeof(buff)); 
+				retval=strlen(buff);
+				send(sockfd, &retval, sizeof(int), FLAGS);
+				for(;;){
+					//Option Selector
+					read(sockfd, buff, sizeof(buff));
+					//Option Selector 1 - List Files
+					if (strncmp("1\n", buff, 2) == 0) {
+						d = opendir(path);
+						//Le cada nome de arquivo no diretorio que corresponde ao username do cliente e envia para impressao no cliente
+					    while ((dir = readdir(d)) != NULL){
+					    	strcpy(buff,dir->d_name);
+				        	write(sockfd, buff, sizeof(buff));
+				        	bzero(buff, MAX);
+				    	}
+				    	closedir(d);
+						//Envia a mensagem dizendo que terminou a listagem
+						write(sockfd, "-1", 2);
+					}
+					//Option Selector 2 - Receive File from Client
+					else if (strncmp("2\n", buff, 2) == 0) {
+						read(sockfd, buff, sizeof(buff));
+						//Recebido um nome valido, procede a receber os dados
+						//A string \\/ eh uma mensagem de erro que o cliente envia quando ha erro de leitura no seu lado
+						if(strcmp("\\\\//", buff)) {
+							printf("Receiving\n");
+							bzero(file_path, 40);
+							strcat(file_path, path);
+							strcat(file_path, "/");
+							strcat(file_path, buff);
+							fp = fopen(file_path, "w");
+							bzero(buff, MAX);
+							read(sockfd, buff, sizeof(buff));
+							fprintf(fp, "%s", buff);
+							bzero(buff, MAX);
+							fclose(fp);
+						}
+					}
+					//Option Selector 3 - Send File to Client
+					else if (strncmp("3\n", buff, 2) == 0) {
+						bzero(buff, sizeof(buff));
+						read(sockfd, buff, sizeof(buff));
+						bzero(file_path, 40);
+						strcat(file_path, path);
+						strcat(file_path, "/");
+						strcat(file_path, buff);
+						fp = fopen(file_path, "r");
+						//Envia apenas se o arquivo existe, senao envia uma mensagem de erro
+						if (fp == NULL) {
+							char *errval = "Not found";
+							send(sockfd, errval, strlen(errval)+1, FLAGS);
+						} else {
+							send_file(fp, sockfd);
+							fclose(fp);
+						}
+					}
+					//Option Selector 4 - Remove Single File
+					else if (strncmp("4\n", buff, 2) == 0) {
+						//Recebe o nome do arquivo e o deleta
+						read(sockfd, buff, sizeof(buff));
+						bzero(file_path, 40);
+						strcat(file_path, path);
+						strcat(file_path, "/");
+						strcat(file_path, buff);
+						remove(file_path);
+					}
+					//Option Selector 5 - Remove All Files
+					else if (strncmp("5\n", buff, 2) == 0) {
+						delete_files_from_user(path);
+					}
+					//Option Selector 6 - Remove Account
+					else if (strncmp("6\n", buff, 2) == 0) {
+						delete_files_from_user(path);
+						strcat(file_path, path);
+						strcat(file_path, "/");
+						rmdir(file_path);
+						closedir(p);
+						break;
+					}
+					//Option Selector 7 - Logout
+					else if (strncmp("7\n", buff, 2) == 0) {
+						closedir(p);
+						break;
+					}
+				}
+			} else {
+				retval = -1;
+				send(sockfd, &retval, sizeof(int), FLAGS);
+			}
+		}
+		// 0 - Criar Conta
+		else if (strncmp("0\n", buff, 2) == 0) {
+			//Lendo o novo usuario e criando a conta
+			read(sockfd, buff, sizeof(buff));
+			retval = create_account(buff);
+			//Retorna um codigo de sucesso ou erro para o cliente, definido na funcao create account
+            send(sockfd, &retval, sizeof(int), FLAGS);
+		}
+		// 7 - Fechar Conexao
+		else if (strncmp("7\n", buff, 2) == 0) {
+			break;
+		}
+	}
+
+	pthread_exit(NULL);
 }
 
+// Driver function
 int main(int argc, char *argv[]) {
+	if(argc != 2) {
+		printf("Usage:\n\t%s [Server Port]\n", argv[0]);
+		exit(1);
+	}
 
-    // Criar socket
-    int serv_sock = -1, incoming_sock;
-    socklen_t incoming_len;
-    int opt = 1;
-    struct sockaddr_in server_address, client_address;
-    struct stat db;
-    pthread_t connection_thread;
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli;
+	struct stat db;
+	pthread_t connection;
 
-    if((serv_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Error creating socket. Aborting.\n");
-        return 1;
-    }
-    
-    if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
-        printf("%s\n", strerror(errno));
-    }
+	//Criando socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(1);
+	}
+	printf("Socket successfully created..\n");
+	//Zerando enderecos, para garantir preenchimento correto
+	bzero(&servaddr, sizeof(servaddr));
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(PORT);
+	//Servidor sera ligado a todas as interfaces de rede do sistema
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//Port do servidor, recebido da linha de comando
+	servaddr.sin_port = htons(atoi(argv[1]));
 
-    // Bind
-    if(bind(serv_sock, (struct sockaddr *) &server_address, sizeof(server_address)) != 0) {
-        printf("Error binding socket. %s\n", strerror(errno));
-        return 1;
-    }
+	//Ligando server ao local definido
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+		printf("socket bind failed...\n");
+		exit(1);
+	}
+	printf("Socket successfully bound to port %s..\n", argv[1]);
 
-    // Listen
-    if(listen(serv_sock, 10) == -1) {  //Backlog de 10 conexoes
-        printf("Failed to listen to connections. %s\n", strerror(errno));
-        return 1;
-    }
 
-    //Criando a pasta que guarda os arquivos
+	//Colocando o servidor em modo passivo, para receber conexoes 
+	if ((listen(sockfd, 5)) != 0) {
+		printf("Listen failed...\n");
+		exit(1);
+	}
+	printf("Server listening..\n");
+
+    //Criando o diretorio de armazenamento
     if(stat("db", &db) == -1) {
         if (mkdir("db", 0777) == -1) {
             printf("Error creating database\n");
@@ -112,19 +255,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Server online and awaiting connections.\n");
+	//Loop de funcionamento. Recebe conexoes continuamente, criando threads para sua execucao
+	while(1) {
+		connfd = accept(sockfd, (SA*)&cli, (socklen_t *) &len);
+		if (connfd < 0) {
+			printf("server acccept failed...\n");
+		}
+		else {
+			printf("server acccept the client...\n");
+			//Cada conexao é tratada em uma thread, que libera seus recursos no fim da interacao com o cliente
+			pthread_create(&connection, NULL, handle_connection, (void *) &connfd);
+		}
+	}
 
-    // Accept
-    while(1) {
-        incoming_sock = accept(serv_sock, (struct sockaddr *) &client_address, &incoming_len);
-        if(incoming_sock == -1) {
-            printf("Connection failed to accept. %s\n", strerror(errno));
-        } else {
-        // fazer a troca de msgs em uma thread
-            pthread_create(&connection_thread, NULL, handle_connection, (void *) &incoming_sock);
-        }
-    }
-
-    close(serv_sock);
-    return 0;
+	return 0;
 }
